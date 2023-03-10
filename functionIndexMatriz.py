3842519543e1e1058206f9a894f6a8ef7c05b025
Andres-GMP <al20760260@ite.edu.mx>
2023-03-09 21:08:50 -0800

    feat: New function but its garbage

---
+++ functionIndexMatriz.py
@@ -0,0 +1,14 @@
+import xlrd
+
+book = xlrd.open_workbook('pagina.xls')
+number_sheets = book.nsheets  # Numero de hojas que tiene el libro de excel
+
+
+def obtain_row_index(rowi, page):
+    sheet = book.sheet_by_index(page)
+    indexRow = [num for num in range(sheet.ncols+1)]
+    listaRow = [listaRow for listaRow in sheet.row_values(rowi)]
+    matriz = []
+    matriz.append(indexRow)
+    matriz.append(listaRow)
+    return matriz
